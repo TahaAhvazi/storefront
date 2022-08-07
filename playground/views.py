115f93779c1d29495ade0cae28230110c900312d
Taha <tahax.dev@gmail.com>
2022-08-07 16:31:21 +0430

    Work with Admin panel in Django

--- playground/views.py
+++ playground/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 from store.models import Product, Order, Customer
+from tags.models import TaggedItem
 from django.db.models import Value, F, Func, ExpressionWrapper,DecimalField
+from django.contrib.contenttypes.models import ContentType
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(
-        discounted_price =  discounted_price
+    content_type = ContentType.objects.get_for_model(Product) # This gonna be the Product ID in the contenttype table in the DataBase
+    queryset=TaggedItem.objects.select_related('tag').filter(
+        content_type=content_type,
+        object_id = 1
     )
     return render(request, 'hello.html', {'name': 'Mosh', 'result': list(queryset)})
